87f62040a58193f8d8ba59272b5cfce5b76247f1
zenrsr <ragasandeepreddy25@gmail.com>
2024-10-09 14:25:15 +0530

    Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return <div className="flex items-center justify-center">Hello World</div>;
+};
+
+export default Home;
